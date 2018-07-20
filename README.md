b1cb46aad573da012ef12d5ae37eaebe2d250575
Kaz Utashiro <kaz@utashiro.com>
2018-07-20 11:09:35 +0900

    minil new App-ansifold

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+App::ansifold - It's new $module
+
+# SYNOPSIS
+
+    use App::ansifold;
+
+# DESCRIPTION
+
+App::ansifold is ...
+
+# LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kaz Utashiro <kaz@utashiro.com>
---
+++ lib/App/ansifold.pm
@@ -0,0 +1,39 @@
+package App::ansifold;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+App::ansifold - It's new $module
+
+=head1 SYNOPSIS
+
+    use App::ansifold;
+
+=head1 DESCRIPTION
+
+App::ansifold is ...
+
+=head1 LICENSE
+
+Copyright (C) Kaz Utashiro.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kaz Utashiro E<lt>kaz@utashiro.comE<gt>
+
+=cut
+
